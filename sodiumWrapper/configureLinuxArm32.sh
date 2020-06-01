6ac6046c7c53e350f09478867d7065fe43d96ae4
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-06-01 19:43:21 +0200

    A bit more portable scripts

--- sodiumWrapper/configureLinuxArm32.sh
+++ sodiumWrapper/configureLinuxArm32.sh
@@ -2,8 +2,9 @@
 export PREFIX="$(pwd)/static-arm32"
 #export CC=/usr/bin/arm-none-eabi-gcc
 export TARGET_ARCH=armv7
+export SYSROOT="$HOME/.konan/dependencies/target-sysroot-2-raspberrypi --host=arm-none-eabi"
 export CFLAGS=""
 export LDFLAGS="--specs=nosys.specs"
 cd libsodium
-./configure  --prefix=$PREFIX --with-sysroot=/home/ionspin/.konan/dependencies/target-sysroot-2-raspberrypi --host=arm-none-eabi "$@"
+./configure  --prefix=$PREFIX --with-sysroot=$SYSROOT --host=arm-none-eabi "$@"
 
--- sodiumWrapper/configureLinuxArm64.sh
+++ sodiumWrapper/configureLinuxArm64.sh
@@ -3,6 +3,7 @@ export PREFIX="$(pwd)/static-arm64"
 export CC=/usr/bin/aarch64-linux-gnu-gcc
 export TARGET_ARCH=armv8-a
 export CFLAGS="-Os -march=${TARGET_ARCH}"
+export SYSROOT="$HOME/.konan/dependencies/target-sysroot-1-linux-glibc-arm64/"
 cd libsodium
-./configure  --prefix=$PREFIX --host=aarch64-linux-gnu-gcc --with-sysroot=/home/ionspin/.konan/dependencies/target-sysroot-1-linux-glibc-arm64/ "$@"
+./configure  --prefix=$PREFIX --host=aarch64-linux-gnu-gcc --with-sysroot=$SYSROOT "$@"
 
