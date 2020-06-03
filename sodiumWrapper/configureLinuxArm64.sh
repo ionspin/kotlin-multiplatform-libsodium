e026d1405dc7c0eebc458cace6f1a6ff3ea7748a
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-06-03 23:52:52 +0200

    Building arm64 with clang

--- sodiumWrapper/configureLinuxArm64.sh
+++ sodiumWrapper/configureLinuxArm64.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 export PREFIX="$(pwd)/static-arm64"
-export CC=/usr/bin/aarch64-linux-gnu-gcc
-export CFLAGS="-O3"
-export SYSROOT=$HOME/.konan/dependencies/target-sysroot-1-linux-glibc-arm64/
+export CLANG_BIN=$HOME/.konan/dependencies/clang-llvm-8.0.0-linux-x86-64/bin
+export CC=$HOME/.konan/dependencies/clang-llvm-8.0.0-linux-x86-64/bin/clang
+export SYSROOT=$HOME/.konan/dependencies/target-sysroot-1-linux-glibc-arm64
+export TOOLCHAIN=$HOME/.konan/dependencies/target-gcc-toolchain-3-linux-x86-64/x86_64-unknown-linux-gnu
+#export CFLAGS="-O3 -target aarch64-unknown-linux-gnu --sysroot $SYSROOT -gcc-toolchain $TOOLCHAIN -fuse-ld=$CLANG_BIN/ld.lld -B$CLANG_BIN"
+export CFLAGS="-O3 -target aarch64-unknown-linux-gnu --sysroot $SYSROOT -fuse-ld=$CLANG_BIN/ld.lld"
+export LDFLAGS="-fuse-ld=$CLANG_BIN/ld.lld"
 cd libsodium
-./configure --prefix=$PREFIX --host=aarch64-linux-gnu-gcc --with-sysroot=$SYSROOT "$@"
+./configure --prefix=$PREFIX --with-sysroot=$SYSROOT --host=aarch64-unknown-linux-gnu "$@"
 
