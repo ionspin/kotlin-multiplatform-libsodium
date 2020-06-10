9255bc44faebd95542776b79e6d6a9ff2a52137d
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-06-10 18:28:27 +0200

    Figuring out linux build with sodium

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -1,1 +1,13 @@
-./gradlew build
+#!/bin/sh
+#this will hopefully download all konan dependancies that we use in the build scripts
+./gradlew multiplatform-crypto-api:build
+#now let's build linux deps
+cd sodiumWrapper
+./makeLinux64.sh
+./makeLinuxArm64.sh
+#now we can do the delegated build
+./gradlew multiplatform-crypto-delegated:build
+#and finally pure build
+./gradlew multiplatform-crypto:build
+
+
