afe9832cb17a0fc28b44a5e240ea939ba22bcc7b
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2020-06-13 00:19:28 +0200

    Use GNU_MAKE

--- sodiumWrapper/makeMingwX86-64.sh
+++ sodiumWrapper/makeMingwX86-64.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 ./configureMingw64.sh
 echo "Configure done"
-
+$GNU_MAKE -j4 -C libsodium clean
+$GNU_MAKE -j4 -C libsodium
+$GNU_MAKE -j4 -C libsodium install
+#make -j4 -C libsodium clean
+#make -j4 -C libsodium
+#make -j4 -C libsodium install
