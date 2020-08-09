791d8d2ca73effa2babc0d0e4ae2bd04efbc75f6
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-08-09 15:42:45 +0200

    Fix build script typos and missing configuration

--- macBuildAndPublish-ios.sh
+++ macBuildAndPublish-ios.sh
@@ -4,6 +4,7 @@ set -e
 ./gradlew multiplatform-crypto-api:build
 #now let's build linux deps
 cd sodiumWrapper
+./makeMacosX86-64.sh
 ./makeIos.sh
 #now we can do the delegated build of ios and macos libraries
 cd ..
--- macBuildAndPublish-pure-ios.sh
+++ macBuildAndPublish-pure-ios.sh
@@ -2,7 +2,7 @@ set -e
 #!/bin/sh
 #this will hopefully download all konan dependancies that we use in the build scripts
 ./gradlew multiplatform-crypto-api:build
-./gradlew multiplatform-crypto:publishIos32ArmPublicationToSnapshotRepository \
-multiplatform-crypto:publishIos64ArmPublicationToSnapshotRepository \
+./gradlew multiplatform-crypto:publishIosArm32PublicationToSnapshotRepository \
+multiplatform-crypto:publishIosArm64PublicationToSnapshotRepository \
 multiplatform-crypto:publishIosPublicationToSnapshotRepository
 set +e
