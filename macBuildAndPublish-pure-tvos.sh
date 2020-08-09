bbe2e8429f25adbfc5083a6b1167615dcf1dd364
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-08-09 17:32:58 +0200

    remove delegate from pure publish script

--- macBuildAndPublish-pure-tvos.sh
+++ macBuildAndPublish-pure-tvos.sh
@@ -2,7 +2,7 @@ set -e
 #!/bin/sh
 #this will hopefully download all konan dependancies that we use in the build scripts
 ./gradlew multiplatform-crypto-api:build
-./gradlew multiplatform-crypto-delegated:publishTvosArm64PublicationToSnapshotRepository \
-multiplatform-crypto-delegated:publishTvosX64PublicationToSnapshotRepository
+./gradlew multiplatform-crypto:publishTvosArm64PublicationToSnapshotRepository \
+multiplatform-crypto:publishTvosX64PublicationToSnapshotRepository
 
 set +e
