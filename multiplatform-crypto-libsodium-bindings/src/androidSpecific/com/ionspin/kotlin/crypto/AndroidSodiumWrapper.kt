9962198aadb1f1a77f85565273c24d25effc5dd5
Ugljesa Jovanovic <ionspinf@gmail.com>
2020-08-22 13:53:01 +0200

    Comment out old debug generated code, but keep for short term reference, add android/jvm wrappers, seemingly solve android unit test problems

---
+++ multiplatform-crypto-libsodium-bindings/src/androidSpecific/com/ionspin/kotlin/crypto/AndroidSodiumWrapper.kt
@@ -0,0 +1,11 @@
+package com.ionspin.kotlin.crypto
+
+
+/**
+ * Created by Ugljesa Jovanovic
+ * ugljesa.jovanovic@ionspin.com
+ * on 22-Aug-2020
+ */
+class SodiumWrapper : LazySodiumAndroid {
+
+}
