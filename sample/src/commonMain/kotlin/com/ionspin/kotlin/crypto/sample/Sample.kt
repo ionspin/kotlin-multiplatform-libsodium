a210ef2db3bbb3564eed87ccfa19df0b5d18be96
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2020-10-20 00:32:42 +0200

    It's late

--- sample/src/commonMain/kotlin/com/ionspin/kotlin/crypto/sample/Sample.kt
+++ sample/src/commonMain/kotlin/com/ionspin/kotlin/crypto/sample/Sample.kt
@@ -1,6 +1,8 @@
 package com.ionspin.kotlin.crypto.sample
 
+import com.ionspin.kotlin.crypto.hash.Hash
 import com.ionspin.kotlin.crypto.util.LibsodiumRandom
+import com.ionspin.kotlin.crypto.util.encodeToUByteArray
 import com.ionspin.kotlin.crypto.util.toHexString
 
 
@@ -8,5 +10,10 @@ object Sample {
     fun runSample() {
         val random = LibsodiumRandom.buf(32)
         println("Random: ${random.toHexString()}")
+    }
+
+    fun hashSomething() : String {
+        val hash = Hash.sha512("123".encodeToUByteArray())
+        return "Hash (SHA512) of 123: ${hash.toHexString()}"
     }
 }
