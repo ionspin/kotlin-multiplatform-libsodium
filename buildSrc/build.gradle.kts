9fdefe0bd0b329beb4bbcb2fa9f5b06904cd8a35
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2021-02-06 16:35:18 +0100

    Bump to 1.3.40

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
     implementation("com.android.tools.build:gradle:4.0.2")
 }
 
