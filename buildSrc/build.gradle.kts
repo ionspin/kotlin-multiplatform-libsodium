140947e249df3b058cf3b7fd10d7e6ba5bc539da
Ugljesa Jovanovic <ugljesa.jovanovic@ionspin.com>
2021-09-22 20:41:05 +0200

    Kotlin to 1.3.51, coroutines and serialization update, libsodium update to latest master

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("com.android.tools.build:gradle:4.0.2")
 }
 
--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -15,10 +15,10 @@
  */
 
 object Versions {
-    val kotlinCoroutines = "1.5.0-native-mt"
-    val kotlin = "1.5.21"
-    val kotlinSerialization = "1.0.1"
-    val kotlinSerializationPlugin = "1.5.10"
+    val kotlinCoroutines = "1.5.2-native-mt"
+    val kotlin = "1.5.31"
+    val kotlinSerialization = "1.3.0-RC"
+    val kotlinSerializationPlugin = "1.5.31"
     val atomicfu = "0.14.3-M2-2-SNAPSHOT" //NOTE: my linux arm32 and arm64 build
     val nodePlugin = "1.3.0"
     val dokkaPlugin = "1.5.0"
